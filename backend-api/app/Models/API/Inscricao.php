115c1de0aa5273d87e901d1e690435f14ae08082
Vitor Gabriel <vitor.gabriel@opovodigital.com>
2024-06-07 03:20:22 -0300

    feat: criando os dados que terao na model de inscricao

--- backend-api/app/Models/API/Inscricao.php
+++ backend-api/app/Models/API/Inscricao.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Inscricao extends Model
 {
     use HasFactory;
+
+    protected $table = "tb_inscricao";
+
+    protected $fillable = [
+        'id_evento_tbi',
+        'nome_inscricao_tbi',
+        'cpf_inscricao_tbi',
+        'email_inscricao_tbi',
+        'is_ativo_tbi'
+    ];
 }
