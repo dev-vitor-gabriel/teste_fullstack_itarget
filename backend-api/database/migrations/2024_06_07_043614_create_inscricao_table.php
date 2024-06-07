d19902f8c461e29920cd7956efa149e9a8496c31
Vitor Gabriel <vitor.gabriel@opovodigital.com>
2024-06-07 01:51:02 -0300

    feat: criando migration da tb_inscricao

---
+++ backend-api/database/migrations/2024_06_07_043614_create_inscricao_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tb_inscricao', function (Blueprint $table) {
+            $table->id('id_inscricao_tbi');
+            $table->unsignedBigInteger('id_evento_tbi');
+            $table->string('nome_inscricao_tbi');
+            $table->string('cpf_inscricao_tbi');
+            $table->string('email_inscricao_tbi');
+            $table->boolean('is_ativo_tbi')->default(true);
+            $table->foreign('id_evento_tbi')->references('id_evento_tbe')->on('tb_evento');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tb_inscricao');
+    }
+};
